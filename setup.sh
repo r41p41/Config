4c8fd8774574d41635257a5277d49d9824b8bbb7
Raghav Pande <r41p41@gmail.com>
2017-05-19 15:52:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,14 @@ apt update
 apt-get -yq install libc6:i386 libncurses5:i386 libstdc++6:i386
 apt-get -yq install libc6-dev-i386
 
-apt-get -yq install gcc vim git clang llvm nasm make python-pip
+apt-get -yq install gcc vim git gdb clang llvm nasm make python-pip radare2
 pip install --upgrade pip
+
+exit
 
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 
-git clone https://github.com/radare/radare2
-cd radare2
-./sys/install.sh
-
-cd
 git clone https://github.com/Z3Prover/z3.git
 cd z3
 python scripts/mk_make.py
@@ -26,5 +23,4 @@ python ../scripts/mk_make.py --python
 cd
 wget http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.2-81205-gcc-linux.tar.gz
 tar -xvf pin-3.2-81205-gcc-linux.tar.gz
-
-exit
+rm pin-3.2-81205-gcc-linux.tar.gz
