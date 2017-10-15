cf70af1a856929e7419b34401a6d1d33c4b402ac
Raghav Pande <r41p41@gmail.com>
2017-10-15 03:01:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo dpkg --add-architecture i386
-sudo apt update
+sudo apt get update
 sudo apt-get -yq install libc6:i386 libncurses5:i386 libstdc++6:i386
 sudo apt-get -yq install libc6-dev-i386
 
@@ -10,12 +10,7 @@ sudo pip install --upgrade pwntools
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 
-git clone https://github.com/Z3Prover/z3.git
-cd z3
-python scripts/mk_make.py --python
-cd build
-make
-make install
+sudo pip install z3
 
 cd
 wget http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.2-81205-gcc-linux.tar.gz
