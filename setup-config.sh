5e7b6a1c3d897674b757e055b4a0fec38617c37d
Raghav Pande <r41p41@gmail.com>
2019-06-28 18:18:53 -0700

    Update setup-config.sh

--- setup-config.sh
+++ setup-config.sh
@@ -3,6 +3,9 @@ cp ./.bashrc ~/.bashrc
 cp ./.vimrc ~/.vimrc
 cd
 
+sudo apt install exuberant-ctags
+sudo apt install silversearcher-ag
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/vimproc.vim/
