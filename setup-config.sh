b8d4d0dbd8fba92bb0715287c5675535a3153d4b
Raghav Pande <r41p41@gmail.com>
2019-08-20 13:29:04 -0700

    Update setup-config.sh

--- setup-config.sh
+++ setup-config.sh
@@ -8,6 +8,3 @@ sudo apt install silversearcher-ag
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-cd ~/.vim/bundle/vimproc.vim/
-make
-cd
